a57ff334ee0031b841e7e8ef0972f8b91c1e7172
sasugabriel99 <91940423+sasugabriel99@users.noreply.github.com>
2021-11-02 11:04:08 +0200

    Add files via upload

--- locatie.js
+++ locatie.js
@@ -6,7 +6,7 @@ function on_success(position)
     document.getElementById("acc").innerHTML   =   "Accuracy   = "+position.coords.accuracy;
     document.getElementById("altitude").innerHTML  =  "Altitude  = "+position.coords.altitude;
     var   latlon   =   position.coords.latitude   +   ","   + position.coords.longitude;
-    var    img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyDHWmYZFLPUG3Bs_vKHSUyK-DVso85ShuI";
+    var    img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyAFEuVWq8FY1uG5cTpTe8UBv_bE9rnZNA0";
     document.getElementById("map").innerHTML       =       "<img src='"+img_url+"'>";
 }
 function on_error(e)
